6406b4e4d1300699d7ccb5555e639a48068190b6
Brett Hockey <b.s.hockey@outlook.com>
2024-04-19 14:43:44 +0900

    Added a new horizontal dashboard summary, in the process broke the expandable content, working to fix

--- script.js
+++ script.js
@@ -4,6 +4,23 @@ const headerButtons = document.querySelectorAll('.header-button');
 headerButtons.forEach((header) => {
     header.addEventListener('click', () => {
         const section = header.parentElement;
+        const content = section.querySelector('.intitially-hidden');
+
+        if (content.style.display === 'none' || content.style.display === '') {
+            content.style.display = 'block';
+            section.classList.add('expanded'); // Add the 'expanded' class
+        } else {
+            content.style.display = 'none';
+            section.classList.remove('expanded'); // Remove the 'expanded' class
+        }
+    });
+});
+
+const summaryButtons = document.querySelectorAll('.summary-button');
+
+headerButtons.forEach((header) => {
+    header.addEventListener('click', () => {
+        const section = header.parentElement;
         const content = section.querySelector('.content');
 
         if (content.style.display === 'none' || content.style.display === '') {
