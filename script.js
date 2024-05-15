e192b4d2f65fe494deea6b6fb754c9953211e2a7
Brett Hockey <b.s.hockey@outlook.com>
2024-05-15 16:12:26 +0900

    Got selection of sections working, back to more tutorials now

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 // Code to toggle the visibility of vertically expandable sections
 const headerButtons = document.querySelectorAll('.header-button');
 
-headerButtons.forEach((header) => {
-    header.addEventListener('click', () => {
-        const section = header.parentElement;
+headerButtons.forEach((headerbutton) => {
+    headerbutton.addEventListener('click', () => {
+        const section = headerbutton.parentElement;
         const content = section.querySelector('.content');
 
         if (content.style.display === 'none' || content.style.display === '') {
@@ -19,19 +19,22 @@ headerButtons.forEach((header) => {
 });
 
 // Code to toggle the visibility of the horiontal dashboard sections (IN PROGRESS......)
-const summaryButtons = document.querySelectorAll('.summary-button');
+const sliderTabs = document.querySelectorAll('.slider-tab')
+const allSliderContents = document.querySelectorAll('.slider-content')
 
-summaryButtons.forEach((summaryButton) => {
-    header.addEventListener('click', () => {
-        const contentID = "#".concat(summaryButton.id.split("-").charAt(0), "-summary-content")
-        const content = document.querySelector(contentID);
+function selectSection(targetSection) {
+    console.log("SHould be viewing: ", targetSection)
+    // Hide all related sections
+    for (var i = 0; i < allSliderContents.length; i++) {
+        allSliderContents[i].style.display = "none";
+    }
+    // Show the selected section
+    document.getElementById(targetSection).style.display = "flex";
+}
 
-        if (content.style.display === 'none' || content.style.display === '') {
-            content.style.display = 'block';
-            section.classList.add('expanded'); // Add the 'expanded' class
-        } else {
-            content.style.display = 'none';
-            section.classList.remove('expanded'); // Remove the 'expanded' class
-        }
-    });
-});
+sliderTabs.forEach(function (sliderButton) {
+    let targetID = sliderButton.getAttribute('data-target')
+    sliderButton.addEventListener('click', function() {
+        selectSection(targetID)
+    })
+})
